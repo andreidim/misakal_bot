582fd9f8aa3c7ed5995734660d8985086348d7de
andreidimjg <andrei.dim.jg@gmail.com>
2020-07-19 21:39:53 +0000

    First Versin Retro Achievement Client implemented

---
+++ src/retro-achievement-client.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+class RetroAchiCommand {
+
+  constructor(endPoint, key, cmd, args, converter) {
+
+    this.key = key;
+    this.endPoint = endPoint;
+    this.cmd = cmd;
+    this.args = args;
+    this.converter = converter;
+
+  }
+
+  build(argValues) {
+    this.endPoint += '&key=' + this.key;
+    this.args.forEach((arg, i) => {
+      this.endPoint += '&' + arg + '=' + argValues[i];
+    });
+
+    return this.endPoint;
+  }
+  async execCmd(argValues) {
+    let response;
+    try {
+      response = await axios.get(this.build(argValues));
+    } catch (err) {
+      logger.error('Http error', err);
+    }
+    if (!converter)
+      return this.converter(response);
+    return response;
+
+  }
+
+
+}
+
+export class RetroAchivClient {
+
+
+  constructor() {
+    const ApiKey ='R030CUCmooaDbVD3eBqFhBCeVop6cShW';
+    const EndPointRoot ='https://ra.hfc-essentials.com/';
+
+    this.commands = new Array();
+
+    //Adding TopTen Command
+    addCommand( new RetroAchiCommand(EndPointRoot+'top_ten.php',ApiKey,'/rank',['user']) );
+
+  }
+
+
+  addCommand(cmd) {
+    this.commands.push(cmd);
+  }
+
+  runCommand(cmd, args) {
+    let cmd = this.commands.filter(cmd => cmd.cmd == cmd);
+    if (!cmd) {
+      return "Unknown command, please check avalilable command list"
+    }
+    return cmd.execCmd(args);
+
+
+  }
+
+
+
+}
+
