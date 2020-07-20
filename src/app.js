9dd8628ae28b651cafd14039c7c3133a9867c637
andreidimjg <andrei.dim.jg@gmail.com>
2020-07-20 15:58:18 +0000

    Formatting src code

--- src/app.js
+++ src/app.js
@@ -5,45 +5,36 @@ import RetroAchiCommand from './retro-achievement-client.js';
 const token = '1356899881:AAHF4AWoZVg3Vsp4WogcIsqTDy5Cjst3mGo';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 const retroAchiClient = new RetroAchiCommand();
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
 });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 
-bot.on('message', async  (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  let parts = msg.text.trim().split(' ');
-  let cmd = parts[0];
-
-  let args = null;
-  if(parts.length > 1)
-     args = parts.slice(1);
- console.log(cmd + ' ' + args);
-  let resp =   await retroAchiClient.runCommand(cmd, args);
-  /*resp.then( (result) => {
-   bot.sendMessage(chatId, result);
-  });*/
-  //console.log(resp);
-  bot.sendMessage(chatId, resp);
-
-
-
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    // send a message to the chat acknowledging receipt of their message
+    let parts = msg.text.trim().split(' ');
+    let cmd = parts[0];
+    let args = null;
+    if (parts.length > 1)
+        args = parts.slice(1);
+    console.log(cmd + ' ' + args);
+    let resp = await retroAchiClient.runCommand(cmd, args);
+    bot.sendMessage(chatId, resp);
 
 });
--- src/retro-achievement-client.js
+++ src/retro-achievement-client.js
@@ -2,103 +2,97 @@ import axios from 'axios';
 
 class RetroAchiCommand {
 
-  constructor(endPoint, key, cmd, args, converter) {
+    constructor(endPoint, key, cmd, args, converter) {
 
-    this.key = key;
-    this.endPoint = endPoint;
-    this.cmd = cmd;
-    this.args = args;
-    this.converter = converter;
+        this.key = key;
+        this.endPoint = endPoint;
+        this.cmd = cmd;
+        this.args = args;
+        this.converter = converter;
 
-  }
-
-  build(argValues) {
-   let paramJoin = this.endPoint.indexOf("?") < 0 ? '?'  :'&';
-   let endPoint=this.endPoint;
-    endPoint += paramJoin+ 'key=' + this.key;
-    if (this.args != undefined) {
-      this.args.forEach((arg, i) => {
-        endPoint += '&' + arg + '=' + argValues[i];
-      });
     }
 
-    return endPoint;
-  }
-  async execCmd(argValues) {
-    let response;
-    try {
-      let query = this.build(argValues);
-      console.log("Querying Retro Achievements API: " + query);
-      response = await axios.get(query);
-      response = response.data;
-    } catch (err) {
-      logger.error('Http error', err);
+    build(argValues) {
+        let paramJoin = this.endPoint.indexOf("?") < 0 ? '?' : '&';
+        let endPoint = this.endPoint;
+        endPoint += paramJoin + 'key=' + this.key;
+        if (this.args != undefined) {
+            this.args.forEach((arg, i) => {
+                endPoint += '&' + arg + '=' + argValues[i];
+            });
+        }
+
+        return endPoint;
     }
-    if (this.converter != undefined)
-      return this.converter(response);
-    return response;
+    async execCmd(argValues) {
+        let response;
+        try {
+            let query = this.build(argValues);
+            console.log("Querying Retro Achievements API: " + query);
+            response = await axios.get(query);
+            response = response.data;
+        } catch (err) {
+            logger.error('Http error', err);
+        }
+        if (this.converter != undefined)
+            return this.converter(response);
+        return response;
 
-  }
-
-
+    }
 }
 
 export default class RetroAchivClient {
 
 
-  constructor() {
-    const ApiKey = 'R030CUCmooaDbVD3eBqFhBCeVop6cShW&user=inco';
-    const ApiKeyNoUser = 'R030CUCmooaDbVD3eBqFhBCeVop6cShW';
-    const EndPointRoot = 'https://ra.hfc-essentials.com/';
+    constructor() {
+        const ApiKey = 'R030CUCmooaDbVD3eBqFhBCeVop6cShW&user=inco';
+        const ApiKeyNoUser = 'R030CUCmooaDbVD3eBqFhBCeVop6cShW';
+        const EndPointRoot = 'https://ra.hfc-essentials.com/';
 
-    this.commands = new Array();
+        this.commands = new Array();
 
-    //Adding TopTen Command
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'top_ten.php', ApiKey, '/top10'));
+        //Adding TopTen Command
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'top_ten.php', ApiKey, '/top10'));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'console_id.php', ApiKey, '/consoles'));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'console_id.php', ApiKey, '/consoles'));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_list.php', ApiKey, '/glist', [  'console']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_list.php', ApiKey, '/glist', ['console']));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_info.php', ApiKey, '/ginfo', [  'game']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_info.php', ApiKey, '/ginfo', ['game']));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_info_extended.php', ApiKey, '/ginfoext', ['game']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_info_extended.php', ApiKey, '/ginfoext', ['game']));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_progress.php', ApiKey, '/gprog', [  'game']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'game_progress.php', ApiKey, '/gprog', ['game']));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_progress.php', ApiKey, '/uprog', [  'game']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_progress.php', ApiKey, '/uprog', ['game']));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_rank.php', ApiKey, '/rank', [  'member']));
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_recent.php', ApiKey, '/recent', [ 'member', 'game']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_rank.php', ApiKey, '/rank', ['member']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_recent.php', ApiKey, '/recent', ['member', 'game']));
 
-    this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_summary.php?results=10', ApiKey, '/summary', [ 'member', 'game']));
+        this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_summary.php?results=10', ApiKey, '/summary', ['member', 'game']));
 
-  }
-
-
-  addCommand(cmd) {
-    this.commands.push(cmd);
-  }
-
-  async runCommand(cmd, args) {
-
-    console.log('==>' + cmd + ' args ' + args);
-    let raCmd = this.commands.find(x => x.cmd == cmd);
-    console.log('==>' + raCmd);
-    if (!raCmd) {
-      return "Unknown command, please check avalilable command list"
     }
 
-    if (raCmd.args != null && args == null ||
-      raCmd.args != null && args.length < raCmd.args.length) {
-      return 'Command: ' + raCmd.cmd + ' must have arguments: ' + raCmd.args;
+
+    addCommand(cmd) {
+        this.commands.push(cmd);
     }
-    return await raCmd.execCmd(args);
 
+    async runCommand(cmd, args) {
 
-  }
+        console.log('==>' + cmd + ' args ' + args);
+        let raCmd = this.commands.find(x => x.cmd == cmd);
+        console.log('==>' + raCmd);
+        if (!raCmd) {
+            return "Unknown command, please check avalilable command list"
+        }
 
-
+        if (raCmd.args != null && args == null ||
+            raCmd.args != null && args.length < raCmd.args.length) {
+            return 'Command: ' + raCmd.cmd + ' must have arguments: ' + raCmd.args;
+        }
+        return await raCmd.execCmd(args);
+    }
 
 }
 
