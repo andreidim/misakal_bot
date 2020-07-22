2e803dd8ca00f1d325a207c344903ba44a385ecb
andreidimjg <andrei.dim.jg@gmail.com>
2020-07-22 22:55:08 +0000

    Summary command was moved to its own class since transformer/converter was too big.

--- src/retro-achievement-client.js
+++ src/retro-achievement-client.js
@@ -5,7 +5,7 @@ import "core-js/fn/array/flat-map";
 
 class RetroAchiCommand {
 
-    constructor(endPoint, key, cmd, args, converter) {
+    constructor(endPoint, key, cmd, args, converter,description) {
 
         this.key = key;
         this.endPoint = endPoint;
@@ -15,14 +15,14 @@ class RetroAchiCommand {
         //comand is execute.
         this.userArgs = null;
         //Default Converter, just convert JSON in String.
-        this.converter = (x) => JSON.stringify(x);
-        if (converter)
-            this.converter = converter;
+        this.description = description;
+        this.converter = converter;
 
     }
 
     
-    build(argValues) {
+    buildEndpoint(argValues) {
+
         let paramJoin = this.endPoint.indexOf("?") < 0 ? '?' : '&';
         let endPoint = this.endPoint;
         endPoint += paramJoin + 'key=' + this.key;
@@ -37,10 +37,26 @@ class RetroAchiCommand {
         return endPoint;
     }
 
-    async execCmd(argValues) {
+   /**
+    * This method transforms resp in some other format.
+    * Default implementation is to called converter inline function
+    * pass as parameter if it was passed, if not return String for the
+    * JSON resp.
+    * @param {*} resp a JSON object which represents response for this command.
+    */
+    transform(resp){
+
+      if(this.converter)
+           return this.converter(resp);
+
+      return  JSON.stringify(resp);
+    }
+
+    async run(argValues) {
+
         let response;
         try {
-            let query = this.build(argValues);
+            let query = this.buildEndpoint(argValues);
             console.log("Querying Retro Achievements API: " + query);
             response = await axios.get(query);
             response = response.data;
@@ -52,11 +68,54 @@ class RetroAchiCommand {
             console.error('Http error', err);
             throw err;
         }
-        if (this.converter != undefined)
-            return this.converter(response);
-        return response;
+      
+        return this.transform(response);
 
     }
+}
+
+/**
+ * Summary Command has its own class since transformer
+ * function has a considerable amount of code in order
+ * to proper format the output.
+ */
+class RASummaryCommand extends RetroAchiCommand {
+
+    constructor(endPoint, apiKey){
+
+        super(endPoint + 'user_summary.php?results=10',
+                apiKey, '/summary', ['member'],null, 
+                 'This command retrieves summary info about a memember'+
+                 'Recently Played Games,Recently Achievements, Status' +
+                 ' And Prof. Pic ');
+
+    }
+
+    transform(resp) {
+
+        let ln = '\n' + repStr('_', 33);
+        let output = `<b>${resp.userArgs.member} Recently Played:</b>  ${ln}\n`;
+        let recents = resp.RecentlyPlayed.map(x => `${x.Title}\n Last Played: ${x.LastPlayed} ${ln}\n`).join(' ');
+        output += recents;
+
+        let achievments = `${ln}\n\n<b>${resp.userArgs.member} Recently Achievements</b> ${ln}\n`;
+        let achivList = Object.values(resp.RecentAchievements)
+            .flatMap(x => Object.values(x))
+            .map(x =>
+                ` ${x.Title}\n for game ${x.GameTitle}\n points ${x.Points} ${ln}\n`);
+
+        output += achievments + achivList.join(' ');
+
+        output += `<b>Points:</b> ${resp.Points} `;
+        output += `${ln}<a href='https://retroachievements.org/${resp.UserPic}'>.</a> \n`;
+        output += `<b>Status:</b> ${resp.Status == 'Offline' ? '&#128308;' : '&#9989;'} `;
+
+        return output;
+
+    }
+
+ 
+                
 }
 
 export default class RetroAchivClient {
@@ -101,29 +160,9 @@ export default class RetroAchivClient {
 
         this.addCommand(new RetroAchiCommand(EndPointRoot + 'user_recent.php', ApiKey, '/recent', ['member', 'game']));
 
-        this.addCommand(
-               new RetroAchiCommand(EndPointRoot + 'user_summary.php?results=10', ApiKey,
-                '/summary', ['member'],
-                (x) =>  {
-                        let ln = '\n' + repStr('_',33);
-                        let output = `<b>${x.userArgs.member} Recently Played:</b>  ${ln }\n` ;
-                        let recents =  x.RecentlyPlayed.map(x => `${x.Title}\n Last Played: ${x.LastPlayed} ${ln}\n`).join(' '); 
-                        output += recents;
+        //Resgistering Summary Command which was defined in a separate class.
+        this.addCommand( new RASummaryCommand(EndPointRoot, ApiKey) );
 
-                        let achievments = `${ln}\n\n<b>${x.userArgs.member} Recently Achievements</b> ${ln}\n`;
-                        let achivList = Object.values(x.RecentAchievements)
-                            .flatMap(x => Object.values(x) )
-                                 .map(x =>  
-                                      ` ${x.Title}\n for game ${x.GameTitle}\n points ${x.Points} ${ln }\n` );
-
-                        output += achievments + achivList.join(' ');
-
-                        output += `<b>Points:</b> ${x.Points} `;
-                        output += `${ln} <b>Pic:</b> <a href='https://retroachievements.org/${x.UserPic}'>Pic</a> `;
-                        output += `<b>Status:</b> ${x.Status == 'Offline' ? '&#128308;' :'&#9989;' } `;
-
-                       return output;      
-                 } )  );
 
                 
 
@@ -147,7 +186,7 @@ export default class RetroAchivClient {
             raCmd.args != null && args.length < raCmd.args.length) {
             return 'Command: ' + raCmd.cmd + ' must have arguments: ' + raCmd.args;
         }
-        return await raCmd.execCmd(args);
+        return await raCmd.run(args);
     }
 
 }
